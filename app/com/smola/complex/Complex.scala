c2b1dc12a3554ec02781717d3d7fa6e554dd8723
Alex Smola <alexsmola@Alexs-MacBook-Air.local>
2022-05-05 14:26:22 +0100

    Added further operations for complex number class

--- app/com/smola/complex/Complex.scala
+++ app/com/smola/complex/Complex.scala
@@ -4,15 +4,30 @@ class Complex(re: Double, im: Double) {
   val real = re
   val imaginary = im
 
+  def conjugate(): Complex = {
+    new Complex(this.real, -this.imaginary)
+  }
+
   def +(complexNumber: Complex): Complex = {
     val newReal = complexNumber.real + this.real
     val newImaginary = complexNumber.imaginary + this.imaginary
     new Complex(newReal, newImaginary)
   }
 
+  def -(complexNumber: Complex): Complex = {
+    val newReal = this.real - complexNumber.real
+    val newImaginary = this.imaginary - complexNumber.imaginary
+    new Complex(newReal, newImaginary)
+  }
+
   def *(complexNumber: Complex): Complex = {
     val newReal = complexNumber.real*this.real - complexNumber.imaginary*this.imaginary
     val newImaginary = complexNumber.real*this.imaginary + complexNumber.imaginary*this.real
+    new Complex(newReal, newImaginary)
+  }
+  def /(complexNumber: Complex): Complex = {
+    val newReal = (complexNumber.real*this.real + complexNumber.imaginary*this.imaginary)/(math.pow(this.real, 2) + math.pow(this.imaginary, 2))
+    val newImaginary = (- complexNumber.real*this.imaginary + complexNumber.imaginary*this.real)/(math.pow(this.real, 2) + math.pow(this.imaginary, 2))
     new Complex(newReal, newImaginary)
   }
 
--- test/com/smola/complex/ComplexTest.scala
+++ test/com/smola/complex/ComplexTest.scala
@@ -13,6 +13,22 @@ class ComplexTest extends AnyFunSuite {
     val expected = new Complex(2.0, 2.0)
     assert(actual.equals(expected))
   }
+  test("Can find conjugate of complex number") {
+    // When
+    val actual = underTest.conjugate()
+    // Then
+    val expected = new Complex(1.0, -1.0)
+    assert(actual.equals(expected))
+  }
+  test("Can subtract complex numbers") {
+    // Given
+    val complexNumber = new Complex(1.0, 1.0)
+    // When
+    val actual = underTest - complexNumber
+    // Then
+    val expected = new Complex(0.0, 0.0)
+    assert(actual.equals(expected))
+  }
   test("Can multiply complex numbers") {
     // Given
     val complexNumber = new Complex(1.0, 1.0)
@@ -20,6 +36,15 @@ class ComplexTest extends AnyFunSuite {
     val actual = underTest * complexNumber
     // Then
     val expected = new Complex(0, 2)
+    assert(actual.equals(expected))
+  }
+  test("Can divide complex numbers") {
+    // Given
+    val complexNumber = new Complex(1.0, 1.0)
+    // When
+    val actual = underTest / complexNumber
+    // Then
+    val expected = new Complex(1.0, 0)
     assert(actual.equals(expected))
   }
 
